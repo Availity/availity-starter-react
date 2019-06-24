54f7bf1728f9b927a61337fc44f7d786b16070dd
Kyle Gray <Kyle.Gray@availity.com>
2019-06-24 14:14:02 -0400

    refactor: updated link and added color to heart

--- project/app/components/Footer.js
+++ project/app/components/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Icon from '@availity/icon';
+import Link from '@availity/link';
 
 const Footer = ({ toggleFlippable }) => (
   <p className="text-center mt-3">
-    Made with <Icon title="Click to See Animated Card" name="heart" color="danger" onClick={toggleFlippable} /> by{' '}
-    <a href="http://www.availity.com">Availity</a> 2019
+    Made with <Icon title="Click to See Animated Card" name="heart" color="danger" onClick={toggleFlippable} />{' '}
+    by{' '}
+    <Link url="https://www.availity.com" target="_blank">
+      Availity
+    </Link>{' '}
+    2019
   </p>
 );
 
