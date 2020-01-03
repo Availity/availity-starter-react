4caba99520fac6c1404161bbef53d892fce9ea07
Jordan Young <jordan.young@availity.com>
2020-01-03 10:30:55 -0500

    refactor: added useMemberInfo hook

--- project/app/hooks/index.js
+++ project/app/hooks/index.js
@@ -1,2 +1,3 @@
-export { default as useAppStore } from './useAppStore';
+export { default as useAppStore, useMemberInfo } from './useAppStore';
+export { default as useQueryParams } from './useQueryParams';
 export { default as useStore } from './useStore';
--- project/app/hooks/useAppStore.js
+++ project/app/hooks/useAppStore.js
@@ -3,4 +3,6 @@ import useStore from './useStore';
 
 const useAppStore = dataSelector => useStore(StoreContext, (contextData = {}) => contextData.appStore, dataSelector);
 
+export const useMemberInfo = () => useAppStore(store => [store.memberInfo, store.setMemberInfo]);
+
 export default useAppStore;
