be92491ee5345017a3a1bb4ebbd4cd6b083d5ec8
Jordan Young <jordan.young@availity.com>
2020-02-07 11:01:04 -0500

    feat: moved search form into own component

--- project/app/components/index.js
+++ project/app/components/index.js
@@ -1,3 +1,4 @@
 export { default as FlipCard } from './FlipCard';
 export { default as Footer } from './Footer';
 export { default as MemberInfo } from './MemberInfo';
+export { default as SearchForm } from './SearchForm';
